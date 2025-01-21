473335bc8c2ae66f2f5d5ee9b62c1f0dbdb2564d
Aleksandr528012 <ask528012@gmail.com>
2025-01-22 00:56:00 +0300

    fix code

--- Source.cpp
+++ Source.cpp
@@ -7,47 +7,43 @@ class Counter {
 
 public:
 
-    int incr{ num++ };
-    int decr{ num-- };
-    bool val;
-    bool action;
+    int get_num() { return num; };
+    void set_num(int value) { if (value > 0) num = value; };
+    int incr() { return num++; };
+    int decr() { return num--; };
+    std::string val;
+    std::string action;
 
     Counter(int num) {};
-    Counter() {
-        int get_num() { return num }
-        void set_num(int value) { if (value > 0) num = value; }
-    }
+    Counter() { 1; };
 
     void print_counter() {
 
         std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет : ";
         std::cin >> val;
 
-        if (val = "да") {
+        if (val == "да") {
             std::cout << "Введите начальное значение счётчика: ";
             std::cin >> num;
-            return num;
         };
 
-        std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
-        std::cin >> action;
-        if (action == "+") {
-            incr;
-            return true;
-        };
-        else (action == -) {
-            decr;
-            return true;
-        };
-        else (action == = ) {
-            std::cout << "" << num << std::endl;
-            return true;
-        };
-            else (action == х) {
+        while (action != "х") {
+            std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+            std::cin >> action;
+            if (action == "+") {
+                incr();
+            }
+            else if (action == "-") {
+                decr();
+            }
+            else if (action == "=") {
+                std::cout << "" << num << std::endl;
+            }
+            else if (action == "х") {
                 std::cout << "До свидания! " << std::endl;
-                return true;
-            };
-
+                return;
+            }
+        };
     };
 };
 
@@ -57,12 +53,6 @@ int main(int argc, char** atgv) {
     SetConsoleOutputCP(1251);
 
     Counter count;
-    int a = 1;
 
-
-    while (a == 1) {
-
-        count.print_counter();
-
-    }
+    count.print_counter();
 };
